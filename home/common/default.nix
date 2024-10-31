7670b525e20bede6497a7fccc547ac3a763414ac
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2024-10-31 11:44:23 +1100

    Split desktop and common

--- home/common/default.nix
+++ home/common/default.nix
@@ -5,6 +5,6 @@
 }:
 {
   imports = [
-    ./programs
+    ./common
   ];
 }
--- hosts/traveler/home.nix
+++ hosts/traveler/home.nix
@@ -1,17 +1,6 @@
 { pkgs, username, ... }:
 {
   imports = [
-    ../../home/home.nix
+    ../../home/mac
   ];
-  home = {
-    username = "${username}";
-    homeDirectory = "/Users/${username}";
-  };
-
-  nix = {
-    package = pkgs.nixVersions.latest;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
 }
