1e70310b28a98e17b924e0e7479054aa54d3519f
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2024-11-12 18:51:27 +1100

    Customising helix

--- home/common/common/dev/helix/default.nix
+++ home/common/common/dev/helix/default.nix
@@ -3,6 +3,29 @@
   programs = {
     helix = {
       enable = true;
+      settings = {
+        theme = "catppuccin_macchiato";
+        editor = {
+          bufferline = "always";
+          statusline = {
+            left = [
+              "mode"
+              "spinner"
+              "read-only-indicator"
+              "file-modification-indicator"
+            ];
+            center = [
+              "file-absolute-path"
+              "file-name"
+            ];
+            mode = {
+              normal = "NORMAL";
+              insert = "INSERT";
+              select = "SELECT";
+            };
+          };
+        };
+      };
     };
   };
 }
--- home/common/common/programs/scripts/zellij-sessioniser.sh
+++ home/common/common/programs/scripts/zellij-sessioniser.sh
@@ -18,7 +18,7 @@ if [[ $# -eq 1 ]]; then
     selected_path=$1
 else
     # If no argument was provided, interactively choose a directory
-    selected_path=$(fd . ~/personal ~/work ~/projects --min-depth 1 --max-depth 2 --type d | fzf)
+    selected_path=$(fd . ~/personal ~/work ~/projects --max-depth 1 --type d | fzf)
 fi
 
 # If no directory was selected, exit the script
@@ -38,9 +38,7 @@ if [[ -z $ZELLIJ ]]; then
 	exit 0
 fi
 
-# We're inside zellij so we'll open a new pane and move into the selected directory
-zellij action new-pane
+cd $selected_path
 
-# Hopefully they'll someday support specifying a directory and this won't be as laggy
-# thanks to @msirringhaus for getting this from the community some time ago!
-zellij action write-chars "cd $selected_path" && zellij action write 10
+# -c will make zellij to either create a new session or to attach into an existing one
+zellij attach $session_name -c
