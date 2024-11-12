32da210c92cc721f27ba7d729038a7d9f59c42e5
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2024-11-12 16:31:06 +1100

    Zellij

--- home/common/common/programs/zellij.nix
+++ home/common/common/programs/zellij.nix
@@ -7,10 +7,4 @@
       enableBashIntegration = true;
     };
   };
-  home.file.".config/zellij/zellij-sessionizer.wasm" = {
-    source = builtins.fetchurl {
-      url = "https://github.com/laperlej/zellij-sessionizer/releases/download/v0.4.3/zellij-sessionizer.wasm";
-      hash = "";
-    };
-  };
 }
