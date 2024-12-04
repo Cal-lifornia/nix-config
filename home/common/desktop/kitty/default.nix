034225c71cdcb748ebc7026ce009804e4c347646
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2024-12-04 16:55:16 +1100

    Changed kitty theme field

--- home/common/desktop/kitty/default.nix
+++ home/common/desktop/kitty/default.nix
@@ -23,7 +23,7 @@
     # kitty has catppuccin theme built-in,
     # all the built-in themes are packaged into an extra package named `kitty-themes`
     # and it's installed by home-manager if `theme` is specified.
-    theme = "Catppuccin-Macchiato";
+    themeFile = "Catppuccin-Macchiato";
     font = {
       name = "JetBrainsMono Nerd Font Mono";
       # use different font size on macOS
