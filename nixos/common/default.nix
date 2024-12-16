58bd7f4ecbe9bf8f47c7331b3ab7a56ac70bef3d
William Hobson <willhobson@live.com.au>
2024-12-17 10:39:37 +1100

    Enabling tailscale in nixos

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -134,6 +134,8 @@
     openFirewall = true;
   };
 
+  services.tailscale.enable = true;
+
   environment.systemPackages = with pkgs; [
 
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
