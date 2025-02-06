b5c8536366c8adfd34d960130416143666ef840e
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2025-02-06 11:25:20 +1100

    Updated flake

--- home/linux/desktop/gtk/stylix.nix
+++ home/linux/desktop/gtk/stylix.nix
@@ -1,12 +1,22 @@
-{ stylix, pkgs, ... }:
 {
+  stylix,
+  pkgs,
+  pkgs-stable,
+  ...
+}:
+{
+  home.packages = with pkgs-stable; [
+    base16-schemes
+  ];
+
   stylix = {
     enable = true;
     # autoEnable = true;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schema}/share/themes/catppuccin-macchiato.yaml";
+    base16Scheme = "${pkgs-stable.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
     # targets = {
     #   cavalier.enable = false;
+    #   cava.enable = false;
     # };
   };
 
