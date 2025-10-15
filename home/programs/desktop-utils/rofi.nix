8f6d444d871fa83685a5131d2ed18df2af8e8f60
William Hobson <willhobson@live.com.au>
2025-10-15 16:42:47 +1100

    Using an external rofi theme tool

--- home/programs/desktop-utils/rofi.nix
+++ home/programs/desktop-utils/rofi.nix
@@ -1,18 +1,34 @@
 { pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    rofi
+    rofi-calc
+  ];
   programs = {
+
     rofi = {
-      enable = true;
+      # enable = true;
       location = "center";
       plugins = with pkgs; [
         rofi-calc
       ];
+      modes = [
+        "drun"
+        "window"
+        "run"
+        "ssh"
+        "calc"
+        "keys"
+        "combi"
+        "filebrowser"
+        "recursivebrowser"
+      ];
 
-      # font = "Noto Sans";
-      # catppuccin = {
-      #   enable = true;
-      #   flavor = "macchiato";
-      # };
+      font = "Noto Sans";
     };
   };
+  # catppuccin.rofi = {
+  #   enable = true;
+  #   flavor = "macchiato";
+  # };
 }
