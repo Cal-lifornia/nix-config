645e327c99332022052de0558f56d8fd1731b85a
William Hobson <willhobson@live.com.au>
2025-10-17 18:55:51 +1100

    Could not get working properly

--- home/programs/scripts/vfio-binds.sh
+++ home/programs/scripts/vfio-binds.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-gpu="0000:03:000.0"
-aud="0000:03:000.1"
+gpu="0000:03:00.0"
+aud="0000:03:00.1"
 
 
 gpu_vd="$(cat /sys/bus/pci/devices/$gpu/vendor) $(cat /sys/bus/pci/devices/$gpu/device)"
@@ -9,7 +9,7 @@ aud_vd="$(cat /sys/bus/pci/devices/$aud/vendor) $(cat /sys/bus/pci/devices/$aud/
 function bind_vfio {
 
   echo $gpu > /sys/bus/pci/drivers/amdgpu/unbind
-  echo $aud > /sys/bus/pci/drivers/amdgpu/unbind
+  echo $aud > "/sys/bus/pci/devices/$aud/driver/unbind"
 
   echo 1 > "/sys/bus/pci/devices/$gpu/remove" 
   echo 1 > "/sys/bus/pci/devices/$aud/remove" 
@@ -17,15 +17,15 @@ function bind_vfio {
   echo 1 > "/sys/bus/pci/rescan"
 
   echo $gpu > /sys/bus/pci/drivers/amdgpu/unbind
-  echo $aud > /sys/bus/pci/drivers/amdgpu/unbind
+  echo $aud > "/sys/bus/pci/devices/$aud/driver/unbind"
 
   echo "$gpu_vd" > /sys/bus/pci/drivers/vfio-pci/new_id
   echo "$aud_vd" > /sys/bus/pci/drivers/vfio-pci/new_id
 }
 
 function unbind_vfio {
-  echo "$gpu_vd" > /sys/bus/pci/drivers/vfio-pci/remove_id
-  echo "$aud_vd" > /sys/bus/pci/drivers/vfio-pci/remove_id
+  echo "$gpu_vd" > "/sys/bus/pci/drivers/vfio-pci/remove_id"
+  echo "$aud_vd" > "/sys/bus/pci/drivers/vfio-pci/remove_id"
 
 
   echo 1 > "/sys/bus/pci/devices/$gpu/remove" 
